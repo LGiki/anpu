bedb2bd859bd2a92a0b7737271b39a7870cf71dd
LGiki <qq547221432@gmail.com>
2024-09-08 22:23:49 +0800

    Added durationToHms function

--- src/utils.ts
+++ src/utils.ts
@@ -3,12 +3,20 @@ import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
 
 export function slugify(title: string) {
-    return limax(title, {
-        tone: false,
-        custom: ['/', '-']
-    })
+  return limax(title, {
+    tone: false,
+    custom: ['/', '-']
+  })
 }
 
 export function cn(...inputs: ClassValue[]) {
-    return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs))
+}
+
+export function durationToHms(duration: number): string {
+  var hours = Math.floor(duration / 3600);
+  var minutes = Math.floor(duration % 3600 / 60);
+  var seconds = Math.floor(duration % 3600 % 60);
+
+  return `${hours > 0 ? `${hours.toString().padStart(2, '0')}:` : ''}${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`
 }
