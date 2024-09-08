268bba2a732ca2497160bbfa7d3b69f7dbe5efaa
LGiki <qq547221432@gmail.com>
2024-09-08 22:11:36 +0800

    Added components

---
+++ src/i18n/ui.ts
@@ -0,0 +1,39 @@
+export const languages = {
+    'zh-hans': '简体中文',
+    'zh-hant': '繁體中文'
+}
+
+export const defaultLang = 'zh-hans'
+
+export const ui = {
+    'zh-hans': {
+        'lyricFontSizeSwitcher.enlarge': '放大字体',
+        'lyricFontSizeSwitcher.reduce': '缩小字体',
+        'songMetadata.album': '专辑',
+        'songMetadata.duration': '时长',
+        'songMetadata.artist': '歌手',
+        'songMetadata.lyricist': '作词',
+        'songMetadata.arranger': '编曲',
+        'songMetadata.composer': '作曲',
+        'songMetadata.producer': '制作人',
+        'songMetadata.releaseDate': '发布日期',
+        'songMetadata.maxingEngineer': '混音',
+        'banlamToneSwitcher.showBanlamTone': '显示台语注音',
+        'banlamToneSwitcher.hideBanlamTone': '隐藏台语注音',
+    },
+    'zh-hant': {
+        'lyricFontSizeSwitcher.enlarge': '放大字體',
+        'lyricFontSizeSwitcher.reduce': '縮小字體',
+        'songMetadata.album': '專輯',
+        'songMetadata.duration': '時長',
+        'songMetadata.artist': '歌手',
+        'songMetadata.lyricist': '作詞',
+        'songMetadata.arranger': '編曲',
+        'songMetadata.composer': '作曲',
+        'songMetadata.producer': '製作人',
+        'songMetadata.releaseDate': '發佈日期',
+        'songMetadata.maxingEngineer': '混音',
+        'banlamToneSwitcher.showBanlamTone': '顯示台語註音',
+        'banlamToneSwitcher.hideBanlamTone': '隱藏台語註音',
+    }
+}
