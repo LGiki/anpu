fdf1df34aa0f2d9c54745d223288432195e38505
LGiki <qq547221432@gmail.com>
2024-10-13 17:25:54 +0800

    Fixed broken links

--- src/utils.ts
+++ src/utils.ts
@@ -8,23 +8,29 @@ import { ui } from './i18n/ui'
 export function getCollectionNameFromUrl(url: URL) {
   // URL 可能为 /album/my-life-will 或 /zh-hant/album/my-life-will
   const [, segment1, segment2] = url.pathname.split('/');
-  
+
   // 如果 urlSegment1 为 i18n locale，说明 urlSegment2 就是 collection name
   if (segment1 in ui) {
     return segment2 || 'song';
   }
-  
+
   return segment1 || 'song';
 }
 
 export function slugify(title: string) {
-  return limax(title.replaceAll(' ', '-'), {
-    tone: false,
-    custom: {
-      '-': '-',
-      ',': '',
-    }
-  })
+  return limax(
+    title
+      .replaceAll(' ', '-')
+      .replace('關於', 'guan-yu')
+      .replace('模樣', 'mu-yang')
+      .replace('怎麼', 'zen-me'),
+    {
+      tone: false,
+      custom: {
+        '-': '-',
+        ',': '',
+      }
+    })
 }
 
 export function cn(...inputs: ClassValue[]) {
