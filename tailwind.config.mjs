41f4253bb54a937a58b546f98209b42356ff6169
LGiki <qq547221432@gmail.com>
2025-06-29 22:51:24 +0800

    Add more share card background colors

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -7,6 +7,10 @@ export default {
         'rainbow-1': 'linear-gradient(135deg, #e1dbff, #d5eeff, #feffdb, #e8fad7, #ffe4cc, #ffcccc)',
         'rainbow-2': 'linear-gradient(135deg, #BCC4FF, #B1E9FF, #BCFFEE, #FEFFDD, #D1FF9B)',
         'rainbow-3': 'linear-gradient(135deg, #87D8FD, #BBE9A5, #FFFBAC, #FFC79A, #FF9DA8)',
+        'rainbow-4': 'linear-gradient(135deg, #FF8080, #FFCF96, #F6FDC3, #FFC79A, #CDFAD5)',
+        'rainbow-5':
+          'linear-gradient(135deg, #fec5bb, #fcd5ce, #fae1dd, #f8edeb, #e8e8e4, #d8e2dc, #ece4db, #ffe5d9, #ffd7ba, #fec89a)',
+        'rainbow-6': 'linear-gradient(135deg, #e2cfc4, #f7d9c4, #faedcb, #c6def1, #dbcdf0, #f2c6de, #f9c6c9)',
         'purple-1': 'linear-gradient(135deg, #957DAD, #E0BBE4, #D291BC, #FFDFD3)',
         dawn: 'linear-gradient(45deg, #7E5072, #AB5A74, #E27E7E, #EA9E79, #E9B661, #EB8126)',
         fire: 'linear-gradient(45deg, #F5BCBC, #FAD1B6, #FFE0BD, #FFE8C7, #F7F2DA)',
